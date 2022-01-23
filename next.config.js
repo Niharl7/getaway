6543d8e2b55fa752e468311a6fedfc6d5aeba185
Niharl7 <97958153+Niharl7@users.noreply.github.com>
2022-01-23 23:49:58 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,28 +1,30 @@
 module.exports = {
   env: {
-    DB_LOCAL_URI: "",
-    DB_URI: "",
+    DB_LOCAL_URI: "mongodb://localhost:27017/getaway",
+    DB_URI:
+      "mongodb+srv://nihar:nZhZ6bsGbyYz6zGQ@getaway.oojom.mongodb.net/getaway?retryWrites=true&w=majority",
 
-    STRIPE_API_KEY: "",
-    STRIPE_SECRET_KEY: "",
+    STRIPE_API_KEY:
+      "pk_test_51KKrwjSI8B2j5UyCxawofmijEoodv6idX6AI3XyPNFxWyVgQQzGC9pYTBI2XyMQRgK0gKdPDi1rek97kiBUZiAli00fHSEAymj",
+    STRIPE_SECRET_KEY:
+      "sk_test_51KKrwjSI8B2j5UyCbUOTWiAY02SX9zqX86sEvfZ03gEYcz4Y5hPP8uRxrCS8AWnqkLAN6yl2xmjoTxrQngtNq3UI00ccJmFf6N",
 
-    STRIPE_WEBHOOK_SECRET: "",
+    STRIPE_WEBHOOK_SECRET: "we_1KL1nBSI8B2j5UyCj44eI4M6",
+    
+    CLOUDINARY_CLOUD_NAME: "getaway",
+    CLOUDINARY_API_KEY: "599735549983952",
+    CLOUDINARY_API_SECRET: "Oe6jUaoM-5hqh_dHVaplHaREHpM",
 
-    CLOUDINARY_CLOUD_NAME: "",
-    CLOUDINARY_API_KEY: "",
-    CLOUDINARY_API_SECRET: "",
+    SMTP_HOST: "smtp.mailtrap.io",
+    SMTP_PORT: "2525",
+    SMTP_USER: "4d82be05b04819",
+    SMTP_PASSWORD: "ae2bfbdddc4419",
+    SMTP_FROM_EMAIL: "noreply@getaway.com",
+    SMTP_FROM_NAME: "Getaway",
 
-    SMTP_HOST: "",
-    SMTP_PORT: "",
-    SMTP_USER: "",
-    SMTP_PASSWORD: "",
-    SMTP_FROM_EMAIL: "",
-    SMTP_FROM_NAME: "",
-
-    NEXTAUTH_URL: "",
+    NEXTAUTH_URL: "https://getaway.vercel.app",
   },
   images: {
-    domains: [""],
+    domains: ["res.cloudinary.com"],
   },
-
 };
